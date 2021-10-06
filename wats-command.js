265f37c9530c75e098a0606c4cf96211a255cd5b
Julian M.N. Bourne <julian.m.n.bourne@planloft.com>
2021-10-06 19:42:12 +0000

    Release 2021-10-06 v1.0.2
    Fixes #3.  Attention to whitespace.  Changes defaults and required tsconfig.

--- wats-command.js
+++ wats-command.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 require('./wats.js').executeIn(process.cwd(), ...((...args) => {
-		args.shift(); // skip over node
+    args.shift(); // skip over node
 
-		while (args.length && args[0].startsWith("-")) {
-			args.shift(); // skip over node option
-		}
+    while (args.length && args[0].startsWith("-")) {
+      args.shift(); // skip over node option
+    }
 
-		if (args.length) {
-			args.shift(); // skip over this command
-		}
+    if (args.length) {
+      args.shift(); // skip over this command
+    }
 
-		return (args);
-	})(...process.argv));
+    return (args);
+  })(...process.argv));
